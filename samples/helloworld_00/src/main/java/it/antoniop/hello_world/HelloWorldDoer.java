0f3bdd4c40196e49389cb2870dd4c345d6651e48
apiovesan <antonio.piovesan@datalogic.com>
2020-05-04 01:39:53 +0200

    DEV .... webinar 01

--- samples/helloworld_00/src/main/java/it/antoniop/hello_world/HelloWorldDoer.java
+++ samples/helloworld_00/src/main/java/it/antoniop/hello_world/HelloWorldDoer.java
@@ -1,16 +1,35 @@
 package it.antoniop.hello_world;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldDoer {
-
-	@RequestMapping(path = "/sayHello")
+	
+    @RequestMapping(path = "/sayHello")
 	public String sayHello(@RequestParam(value = "name", defaultValue = "Marco", required = false) String name) {
 		//
 		return "Hello to SpringBoot World! Welcome <span style='font-weight: bold; color: blue'>" + name + " ... </span>";
 	}
+	
+    
+
+    @Value("${default.surname}")
+    private String surname;
+    
+    @Value("${default.name}")
+    private String name;
+
+	
+	
+	@RequestMapping(path = "/sayHi")
+	public String sayHiProperly(@RequestParam(value = "name", required = false) String inName) {
+		//
+		String theName = inName==null ? this.name : inName;
+		return "Hello to SpringBoot World! Welcome <span style='font-weight: bold; color: blue'>" + theName + " " + surname + " ... </span>";
+	}
+	
 }
