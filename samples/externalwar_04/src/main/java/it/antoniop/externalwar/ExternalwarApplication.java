caa77d167d4aad02fcbc53b0d634259e130361e2
apiovesan <antonio.piovesan@datalogic.com>
2020-05-04 17:41:36 +0200

    DEV

--- samples/externalwar_04/src/main/java/it/antoniop/externalwar/ExternalwarApplication.java
+++ samples/externalwar_04/src/main/java/it/antoniop/externalwar/ExternalwarApplication.java
@@ -6,17 +6,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExternalwarApplication   /* extends SpringBootServletInitializer */  {
+public class ExternalwarApplication   /* * / extends SpringBootServletInitializer / * */  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExternalwarApplication.class, args);
 	}
 
 
-	/*
+	/* *
 		@Override
 		protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 			return super.configure(builder);
 		} 
-	*/
+	 * */
 }
--- samples/externalwar_04/src/main/java/it/antoniop/externalwar/ServletInitializer.java
+++ samples/externalwar_04/src/main/java/it/antoniop/externalwar/ServletInitializer.java
@@ -3,9 +3,9 @@ package it.antoniop.externalwar;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer /* */  extends SpringBootServletInitializer  /* */  {
 
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ExternalwarApplication.class);
