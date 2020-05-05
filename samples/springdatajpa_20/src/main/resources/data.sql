520fed5ad798254b6659867a50d42ace5ff25300
apiovesan <apiovesan@W7-APIOVESAN.dl.net>
2020-05-05 23:20:52 +0200

    DEV: sample projects for spring boot webinar 01

--- samples/springdatajpa_20/src/main/resources/data.sql
+++ samples/springdatajpa_20/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 USE test;
 
 INSERT INTO  TBL_EMPLOYEES (first_name, last_name, email)  VALUES	('Marco', 'Rossi', 'mrossi@gmail.com');
-INSERT INTO  TBL_EMPLOYEES (first_name, last_name, email)  VALUES  	('Lorenzo', 'Bianco', 'lorenzo@email.com');
+INSERT INTO  TBL_EMPLOYEES (first_name, last_name, email)  VALUES  	('Lorenzo', 'Bianchi', 'lorenzo@email.com');
 INSERT INTO  TBL_EMPLOYEES (first_name, last_name, email)  VALUES	('Joe', 'Satriani', 'joe@satch.com');
