e360b71975391beae66abad2a6c28c57a12975c2
Piovesan <antonio.piovesan@datalogic.com>
2020-11-07 01:22:46 +0100

    DEV: fix removing useless commented import lines

--- samples/externalwar_04/src/main/java/it/antoniop/externalwar/ExternalwarApplication.java
+++ samples/externalwar_04/src/main/java/it/antoniop/externalwar/ExternalwarApplication.java
@@ -2,8 +2,8 @@ package it.antoniop.externalwar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+// import org.springframework.boot.builder.SpringApplicationBuilder;
+// import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ExternalwarApplication   /* * / extends SpringBootServletInitializer / * */  {
